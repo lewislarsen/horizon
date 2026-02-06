2818cbdcaebd5b1ca1e3814bb845eee1ac0f6f83
Lewis Larsen <lewis@larsens.dev>
2026-02-06 08:01:32 +0000

    wip

--- resources/js/screens/batches/preview.vue
+++ resources/js/screens/batches/preview.vue
@@ -45,18 +45,23 @@ export default {
       </div>
 
       <div class="bg-default shadow-xs-with-border rounded-lg p-5">
-        <div v-if="!ready" class="flex justify-center"><svg class="size-6 animate-spin text-icon-alpha" viewBox="0 0 20 20" fill="currentColor"><path d="M12 10a2 2 0 0 1-3.41 1.41A2 2 0 0 1 10 8V0a9.97 9.97 0 0 1 10 10h-8zm7.9 1.41A10 10 0 1 1 8.59.1v2.03a8 8 0 1 0 9.29 9.29h2.02zm-4.07 0a6 6 0 1 1-7.25-7.25v2.1a3.99 3.99 0 0 0-1.4 6.57 4 4 0 0 0 6.56-1.42h2.1z"/></svg></div>
+        <div v-if="!ready" class="flex justify-center">
+          <svg aria-hidden="true" class="size-5 shrink-0 mr-2 text-icon-alpha animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+        </div>
 
         <div v-if="ready" class="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-4">
           <div class="space-y-4">
-            <div><label class="text-xssm font-medium text-weak uppercase tracking-wider block">Batch ID</label><div class="text-sm font-medium text-strong">{{ batch.id }}</div></div>
-            <div><label class="text-xssm font-medium text-weak uppercase tracking-wider block">Created</label><div class="text-sm text-default">{{ formatDateIso(batch.createdAt).format('YYYY-MM-DD HH:mm:ss') }}</div></div>
-            <div v-if="batch.finishedAt"><label class="text-xssm font-medium text-weak uppercase tracking-wider block">Finished</label><div class="text-sm text-default">{{ formatDateIso(batch.finishedAt).format('YYYY-MM-DD HH:mm:ss') }}</div></div>
+            <div><label class="text-xssm font-medium text-weak tracking-wider block">Batch ID</label><div class="text-sm font-medium text-strong">{{ batch.id }}</div></div>
+            <div><label class="text-xssm font-medium text-weak tracking-wider block">Created</label><div class="text-sm text-default">{{ formatDateIso(batch.createdAt).format('YYYY-MM-DD HH:mm:ss') }}</div></div>
+            <div v-if="batch.finishedAt"><label class="text-xssm font-medium text-weak tracking-wider block">Finished</label><div class="text-sm text-default">{{ formatDateIso(batch.finishedAt).format('YYYY-MM-DD HH:mm:ss') }}</div></div>
           </div>
           <div class="space-y-4">
-            <div><label class="text-xssm font-medium text-weak uppercase tracking-wider block">Progress</label><div class="text-sm font-medium text-strong">{{ batch.progress }}% ({{ batch.processedJobs }} / {{ batch.totalJobs }} jobs)</div></div>
-            <div><label class="text-xssm font-medium text-weak uppercase tracking-wider block">Queue</label><div class="text-sm text-default">{{ batch.options.queue || '-' }}</div></div>
-            <div><label class="text-xssm font-medium text-weak uppercase tracking-wider block">Failed Jobs</label><div class="text-sm" :class="batch.failedJobs > 0 ? 'text-danger font-medium' : 'text-default'">{{ batch.failedJobs }}</div></div>
+            <div><label class="text-xssm font-medium text-weak tracking-wider block">Progress</label><div class="text-sm font-medium text-strong">{{ batch.progress }}% ({{ batch.processedJobs }} / {{ batch.totalJobs }} jobs)</div></div>
+            <div><label class="text-xssm font-medium text-weak tracking-wider block">Queue</label><div class="text-sm text-default">{{ batch.options.queue || '-' }}</div></div>
+            <div><label class="text-xssm font-medium text-weak tracking-wider block">Failed Jobs</label><div class="text-sm" :class="batch.failedJobs > 0 ? 'text-danger font-medium' : 'text-default'">{{ batch.failedJobs }}</div></div>
           </div>
         </div>
       </div>
